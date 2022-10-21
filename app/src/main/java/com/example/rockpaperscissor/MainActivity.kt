7b20eb3c19f4a4fbbc287bdfa18dd03950386bf0
cindy <cindywawa91@gmail.com>
2022-10-21 14:43:36 +0800

    1

---
+++ app/src/main/java/com/example/rockpaperscissor/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.rockpaperscissor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //變數與XML元件綁定
+
+    }
+}
